4a9cd192160c70f5f13fd5e719321d905618ff69
D1msn <flash.volkov@gmail.com>
2021-10-28 00:06:53 +0300

    создание роутов, стилизация

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import './App.scss';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Profile from "./components/MainContent/Profile";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Profile from "./components/Profile/Profile";
+import SideBar from "./components/SideBar/SideBar";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<div className={"app-wrapper"}>
-				<Sidebar/>
-				<Header/>
-				<div className={"main"}>
-					<div className={"main__content"}>
-						<Profile/>
-						<Dialogs/>
+		<BrowserRouter>
+			<div className="App">
+				<div className={"app-wrapper"}>
+					<SideBar/>
+					<Header/>
+					<div className={"main"}>
+						<div className={"main__content"}>
+							<Route path={"/profile"} component={Profile}/>
+							<Route path={"/dialogs"} component={Dialogs}/>
+						</div>
 					</div>
+					<Footer/>
 				</div>
-				<Footer/>
 			</div>
-		</div>
+		</BrowserRouter>
+
 	);
 }
 
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import {ReactSVG} from "react-svg";
+
+
+import profileIcon from '../../images/user_24.svg'
+import messagesIcon from '../../images/chating_24.svg'
+import newsIcon from '../../images/choose_type-public.svg'
+import musicIcon from '../../images/volume-up.svg'
+import settingsIcon from '../../images/settings_24.svg'
+
+import sidebarAvatar from '../../images/ava.jpg'
+
+import './SideBar.scss';
+
+const SideBar = () => {
+
+	return (
+
+
+		<nav className="sidebar ">
+			<div className="sidebar__avatar">
+				<NavLink to="/profile">
+					<img src={sidebarAvatar} alt="Avatar"/>
+				</NavLink>
+			</div>
+			<ul className="sidebar__links">
+				<li className="sidebar__item">
+					<NavLink to="/profile" className="sidebar__link">
+						<ReactSVG src={profileIcon} className="sidebar__item-icon"/>
+						Profile
+					</NavLink>
+				</li>
+				<li className="sidebar__item">
+					<NavLink to="/dialogs" className="sidebar__link">
+						<ReactSVG src={messagesIcon} className="sidebar__item-icon"/>
+						Messages
+					</NavLink>
+				</li>
+				<li className="sidebar__item">
+					<NavLink to="/news" className="sidebar__link">
+						<ReactSVG src={newsIcon} className="sidebar__item-icon"/>
+						News
+					</NavLink></li>
+				<li className="sidebar__item">
+					<NavLink to="/music" className="sidebar__link">
+						<ReactSVG src={musicIcon} className="sidebar__item-icon"/>
+						Music
+					</NavLink>
+				</li>
+				<li className="sidebar__item">
+					<NavLink to="/settings" className="sidebar__link">
+						<ReactSVG src={settingsIcon} className="sidebar__item-icon"/>
+						Settings
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+
+
+	)
+		;
+};
+
+export default SideBar;
